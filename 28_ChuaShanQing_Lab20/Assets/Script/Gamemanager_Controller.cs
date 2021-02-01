555e0af1b1ef51ca3ac3785a8f4130e3d93161c7
KanouIzumi <shanqing649@gmail.com>
2021-02-01 11:54:45 +0800

    Lab20

---
+++ 28_ChuaShanQing_Lab20/Assets/Script/Gamemanager_Controller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemanager_Controller : MonoBehaviour
+{
+    public static Gamemanager_Controller instance;
+    public GameObject Wall;
+    int numberOfSpawn = 200;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        for (int i = 0; i < numberOfSpawn; i++)
+        {
+            Vector3 randomPos = new Vector3(Random.Range(-25, 25), 0.5f, Random.Range(-25, 25));
+            Instantiate(Wall, randomPos, Quaternion.identity);
+        }
+        if (instance == null)
+        {
+            instance = this;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
